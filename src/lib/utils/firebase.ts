69b9e53dd714f01a353c99a461229b27ee2818ca
wingstako <wingstako@proton.me>
2023-02-02 14:49:34 +0800

    fix: remove firebase

--- src/lib/utils/firebase.ts
+++ src/lib/utils/firebase.ts
@@ -1,34 +1,34 @@
 
 
-import { dev, browser } from '$app/environment'
+// import { dev, browser } from '$app/environment'
 
-import { initializeApp } from 'firebase/app'
-import { getAnalytics, type Analytics } from 'firebase/analytics'
-import { getPerformance, type FirebasePerformance } from 'firebase/performance'
-import { getDatabase, type Database } from 'firebase/database';
+// import { initializeApp } from 'firebase/app'
+// import { getAnalytics, type Analytics } from 'firebase/analytics'
+// import { getPerformance, type FirebasePerformance } from 'firebase/performance'
+// import { getDatabase, type Database } from 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyD6UE0Cl-NNsveeBrAFYFMNHTYbrL41Ir8',
-    authDomain: 'wingstako-dev-blog.firebaseapp.com',
-    projectId: 'wingstako-dev-blog',
-    storageBucket: 'wingstako-dev-blog.appspot.com',
-    messagingSenderId: '738823241655',
-    appId: '1:738823241655:web:d4e230f5e1bc54213bb232',
-    measurementId: 'G-XXEDEX1PTF'
-}
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyD6UE0Cl-NNsveeBrAFYFMNHTYbrL41Ir8',
+//     authDomain: 'wingstako-dev-blog.firebaseapp.com',
+//     projectId: 'wingstako-dev-blog',
+//     storageBucket: 'wingstako-dev-blog.appspot.com',
+//     messagingSenderId: '738823241655',
+//     appId: '1:738823241655:web:d4e230f5e1bc54213bb232',
+//     measurementId: 'G-XXEDEX1PTF'
+// }
 
-export let analytics: Analytics;
-export let performance: FirebasePerformance;
-export let database: Database;
-export const initializeFirebase = () => {
+// export let analytics: Analytics;
+// export let performance: FirebasePerformance;
+// export let database: Database;
+// export const initializeFirebase = () => {
 
-    const app = initializeApp(firebaseConfig)
+//     const app = initializeApp(firebaseConfig)
 
-    database = getDatabase(app)
-    if (!dev && browser) {
-        analytics = getAnalytics(app)
-        performance = getPerformance(app)
-    }
-}
+//     database = getDatabase(app)
+//     if (!dev && browser) {
+//         analytics = getAnalytics(app)
+//         performance = getPerformance(app)
+//     }
+// }
 
 
